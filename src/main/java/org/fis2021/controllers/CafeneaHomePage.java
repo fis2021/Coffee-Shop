e821897a0081bc9e74e58bf6cb4ab0aac7adb94c
MGitHubb <maria.cioara113@yahoo.com>
2021-05-12 12:51:10 +0300

    CS-55 added 'Rezervari/Comenzi' window in 'Vezi Istoric'

--- src/main/java/org/fis2021/controllers/CafeneaHomePage.java
+++ src/main/java/org/fis2021/controllers/CafeneaHomePage.java
@@ -1,4 +1,28 @@
 package org.fis2021.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CafeneaHomePage {
+    @FXML
+    private Button buttonVeziIstoric;
+
+    @FXML
+    public void loadVeziIstoric(){
+        try {
+            Stage stage = (Stage) buttonVeziIstoric.getScene().getWindow();
+            Parent loginRoot = FXMLLoader.load(getClass().getResource("/fxml/VeziIstoric.fxml"));
+            Scene scene = new Scene(loginRoot, 640, 480);
+            stage.setTitle("Vezi Istoric - CoffeeShop");
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
