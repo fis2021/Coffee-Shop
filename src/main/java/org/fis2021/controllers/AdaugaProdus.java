9dda52855e6db7e03d61297b5de723552d773018
MGitHubb <maria.cioara113@yahoo.com>
2021-05-16 19:32:59 +0300

    CS-58 added the rest of the back buttons

--- src/main/java/org/fis2021/controllers/AdaugaProdus.java
+++ src/main/java/org/fis2021/controllers/AdaugaProdus.java
@@ -1,10 +1,17 @@
 package org.fis2021.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 import org.fis2021.exceptions.ProductAlreadyExists;
 import org.fis2021.services.ProductService;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class AdaugaProdus {
     @FXML
@@ -13,6 +20,8 @@ public class AdaugaProdus {
     private TextField pret;
     @FXML
     private Label errMessage;
+    @FXML
+    private Button backButton;
 
     public void initialize(){
         ProductService.initService();
@@ -32,6 +41,19 @@ public class AdaugaProdus {
         }
         catch(ProductAlreadyExists e) {
             errMessage.setText(e.getMessage());
+        }
+    }
+
+    @FXML
+    public void loadModificaMeniu(){
+        try {
+            Stage stage = (Stage) backButton.getScene().getWindow();
+            Parent loginRoot = FXMLLoader.load(getClass().getResource("/fxml/ModificaMeniu.fxml"));
+            Scene scene = new Scene(loginRoot, 640, 480);
+            stage.setTitle("Modifica Meniu - CoffeeShop");
+            stage.setScene(scene);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
